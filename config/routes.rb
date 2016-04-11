e2c0d7ddcc41540a7b3cc837f798891338bcd660
AJ Foster <web@aj-foster.com>
2016-04-11 00:32:44 -0400

    Add preliminary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'entries#index'
+
+  resources :entries
+  resources :categories do
+    member do
+      resources :entries
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
