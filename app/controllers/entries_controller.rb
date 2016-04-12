df40240e4361ca2748c7948ca04fd4cbc0c3e0dc
AJ Foster <web@aj-foster.com>
2016-04-11 23:44:58 -0400

    Add preliminary index/show views.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,11 @@
 class EntriesController < ApplicationController
 
   def index
+    @entries = Entry.order(name: :asc)
   end
 
   def show
+    @entry = Entry.find(params[:id])
   end
 
   def new
